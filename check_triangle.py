c377a5d7346a9b117a378c26b871c1f4dd9f07e6
Tetragon <sushilovvladislav@gmail.com>
2025-10-18 00:43:52 +0300

    Добавление исключений Исправление мелких недочётов

--- check_triangle.py
+++ check_triangle.py
@@ -3,37 +3,43 @@ from math import isclose
 def check_triagnle_type():
     
 
-    print("Программа для определения типа")
+    print("Программа для определения типа треугольника")
     print("=" * 40)
 
-    side1 = float(input("Введите сторону 1: ").strip())
-    side2 = float(input("Введите сторону 2: ").strip())
-    side3 = float(input("Введите сторону 3: ").strip())
+    try:
+        side1 = float(input("Введите сторону 1: ").strip())
+        side2 = float(input("Введите сторону 2: ").strip())
+        side3 = float(input("Введите сторону 3: ").strip())
 
-    sides = sorted([side1, side2, side3])
-    a, b, c = sides
+        sides = sorted([side1, side2, side3])
+        a, b, c = sides
 
-    if a <= 0 or b <= 0 or c <= 0:
-        return "Все стороны должны быть больше нуля"
+        if a <= 0 or b <= 0 or c <= 0:
+            return "Все стороны должны быть больше нуля"
 
-    if a + b <= c or isclose(a + b, c, rel_tol=1e-9):
-        return "Треугольник с такими сторонами не существует"
+        if a + b <= c or isclose(a + b, c, rel_tol=1e-9):
+            return "Треугольник с такими сторонами не существует"
         
-    if isclose(a ** 2 + b ** 2, c ** 2, rel_tol=1e-9):
-        return "Прямоугольный треугольник"
+        if isclose(a ** 2 + b ** 2, c ** 2, rel_tol=1e-9):
+            return "Прямоугольный треугольник"
 
-    elif (isclose(a, b, rel_tol=1e-9) and
-          isclose(b, c, rel_tol=1e-9)):
-        return "Равносторонний треугольник"
+        elif (isclose(a, b, rel_tol=1e-9) and
+            isclose(b, c, rel_tol=1e-9)):
+            return "Равносторонний треугольник"
 
-    elif (isclose(a, b, rel_tol=1e-9) or
-        isclose(b, c, rel_tol=1e-9) or
-        isclose(a, c, rel_tol=1e-9)):
-        return "Равнобедренный треугольник"
+        elif (isclose(a, b, rel_tol=1e-9) or
+            isclose(b, c, rel_tol=1e-9) or
+            isclose(a, c, rel_tol=1e-9)):
+            return "Равнобедренный треугольник"
     
-    else:
-        return "Разносторонний треугольник"
+        else:
+            return "Разносторонний треугольник"
+        
+    except ValueError:
+        return "\nОшибка: Пожалуйста, введите корректные числа (разделитель - точка)"
 
+    except KeyboardInterrupt:
+        return "Программа прервавна пользователем"
 
 
 if __name__ == "__main__":
